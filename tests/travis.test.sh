f259a400287d65e9fef9bb0b88e69ef5cf5cef2a
Justin <justin-p@users.noreply.github.com>
2019-04-24 20:27:57 +0200

    Update travis.test.sh

--- tests/travis.test.sh
+++ tests/travis.test.sh
@@ -1,2 +1,2 @@
 #/bin/bash
-if (($(curl "http://localhost:81/somefile.php?someparam=../../../etc" -s -f -w %{http_code} -o /dev/null)  == 403)); then echo "Request blocked by WAF, Noice!" ;else exit 1; fi
+if (($(curl "http://localhost:81/somefile.php?someparam=../../../etc" -s -f -w %{http_code} -o /dev/null)  == 403)); then exit 0 ;else exit 1; fi
