a897b9097afae4dac2f862fa6927a09a98d7e018
justin-p <justin-p@users.noreply.github.com>
2019-04-24 17:15:19 +0200

    testing

--- tests/travis.test.sh
+++ tests/travis.test.sh
@@ -1,7 +1,2 @@
 #/bin/bash
-http_code=$(curl "http://localhost:81/somefile.php?someparam=../../../etc" -s -f -w %{http_code} -o /dev/null) && 
-if ((http_code == 403)); then 
-	echo "Request blocked by WAF, Noice!" 
-else 
-	exit 1 
-fi
+if (($(curl "http://localhost:81/somefile.php?someparam=../../../etc" -s -f -w %{http_code} -o /dev/null)  == 403)); then echo "Request blocked by WAF, Noice!" ;else exit 1; fi
