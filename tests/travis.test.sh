af4f3365c1f58d1a29d8a05d33256fa3775a57c6
justin-p <justin-p@users.noreply.github.com>
2019-04-25 12:29:29 +0200

    testing

--- tests/travis.test.sh
+++ tests/travis.test.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 sleep 30s
 http_code=$(curl "http://localhost:81/somefile.php?someparam=../../../etc" -s -f -w %{http_code} -o /dev/null)
-if (($http_code == 40)); then exit 0 ;else echo "$http_code"; exit 200; fi
+if (($http_code == 403)); then echo "We got a $http_code. Our WAF blocked the reqeust, Noice!"; exit 0 ;else echo "We got a $http_code. Our WAF did NOT block the request :("; exit 200; fi
