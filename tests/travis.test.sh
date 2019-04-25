422bdc50333d7eba3b43268f5fe5cc46b2c31eb3
justin-p <justin-p@users.noreply.github.com>
2019-04-25 11:51:27 +0200

    testing

--- tests/travis.test.sh
+++ tests/travis.test.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 sleep 30s
-http_code=($(curl "http://localhost:81/somefile.php?someparam=../../../etc" -s -f -w %{http_code} -o /dev/null)
-if ($http_code == 403)); then exit 0 ;else exit $http_code; fi
+http_code=$(curl "http://localhost:81/somefile.php?someparam=../../../etc" -s -f -w %{http_code} -o /dev/null)
+if (($http_code == 403)); then exit 0 ;else echo $http_code exit 1; fi
